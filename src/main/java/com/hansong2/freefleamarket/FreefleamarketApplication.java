99f5c4ad6e53de4f8b2e6f4f279d99122e0a494b
Euiheehan <han419120@naver.com>
2020-06-19 21:19:17 +0900

    'first'

---
+++ src/main/java/com/hansong2/freefleamarket/FreefleamarketApplication.java
@@ -0,0 +1,13 @@
+package com.hansong2.freefleamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreefleamarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreefleamarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansong2/freefleamarket/FreefleamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansong2.freefleamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreefleamarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
